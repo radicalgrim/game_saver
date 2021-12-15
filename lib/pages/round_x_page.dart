d0c94491d9f1a38e3f6c4a66110f4b83dd538f82
Josh <josh.reese.is@gmail.com>
2021-12-15 16:12:10 -0700

    Simplify round_x page

--- lib/pages/round_x_page.dart
+++ lib/pages/round_x_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:game_saver/pages/end_of_round.dart';
 import 'package:game_saver/res/globals.dart' as globals;
 import 'package:flutter/material.dart';
@@ -16,11 +17,181 @@ class RoundXPage extends StatefulWidget {
 
 class RoundXPageState extends State<RoundXPage> {
   final fieldText = TextEditingController();
+  final _formKey = GlobalKey<FormBuilderState>();
 
   final int roundNum = globals.currentGame!.round + 1;
   final int playerIndex = globals.currentGame!.currentPlayer;
 
   int score = 0;
+
+  //PAGE
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ProjectColors.primarySwatch.shade50,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text(ProjectStrings.appTitle),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 50),
+                  child: Text("Round $roundNum",
+                      style: ProjectTextStyles.pageTitleTextStyle),
+                ),
+                Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                      color: ProjectColors.primarySwatch.shade100,
+                      borderRadius: const BorderRadius.all(Radius.circular(8))),
+                  child: globals.currentGame!.playersView[playerIndex],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    FormBuilder(
+                      key: _formKey,
+                      onChanged: () {
+                        _formKey.currentState?.save();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: FormBuilderTextField(
+                          name: "score",
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            constraints: BoxConstraints(
+                              maxWidth: 70,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            filled: true,
+                            hintText: ProjectStrings.roundInputExample,
+                            fillColor: Colors.white70,
+                            isDense: true,
+                          ),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.add_circle),
+                      color: ProjectColors.primarySwatch.shade700,
+                      onPressed: () {
+                        if (_formKey.currentState!.fields["score"]?.value !=
+                            0) {
+                          globals.currentGame!.players[playerIndex].points +=
+                              int.parse(_formKey
+                                  .currentState!.fields["score"]?.value);
+                          nextPlayer();
+                        }
+                      },
+                    ),
+                  ],
+                ),
+                // Row(
+                //   children: [
+                //     Padding(
+                //       padding: const EdgeInsets.fromLTRB(130, 10, 0, 0),
+                //       child: SizedBox(
+                //         width: 100,
+                //         height: 50,
+                //         child: TextField(
+                //           keyboardType: TextInputType.number,
+                //           decoration: const InputDecoration(
+                //             hintText: ProjectStrings.roundInputExample,
+                //             labelText: ProjectStrings.roundInput,
+                //             contentPadding: EdgeInsets.symmetric(
+                //                 horizontal: 20.0, vertical: 10.0),
+                //           ),
+                //           onChanged: (String value) {
+                //             score = int.parse(value);
+                //           },
+                //           controller: fieldText,
+                //         ),
+                //       ),
+                //     ),
+                //     Padding(
+                //         padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                //         child: IconButton(
+                //           icon: const Icon(Icons.add_circle),
+                //           color: ProjectColors.primarySwatch.shade700,
+                //           onPressed: () {
+                //             if (score != 0) {
+                //               globals.currentGame!.players[playerIndex].points +=
+                //                   score;
+                //               score = 0;
+                //               fieldText.clear();
+                //               nextPlayer();
+                //             }
+                //           },
+                //         )),
+                //   ],
+                // ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(25, 20, 0, 0),
+                          child: ElevatedButton(
+                            child: const Text(ProjectStrings.roundPrevious,
+                                style: ProjectTextStyles.buttonLargeTextStyle),
+                            onPressed: () {
+                              previousPlayer();
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                          child: ElevatedButton(
+                            child: const Text(ProjectStrings.roundNext,
+                                style: ProjectTextStyles.buttonLargeTextStyle),
+                            onPressed: () {
+                              nextPlayer();
+                            },
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                  child: ElevatedButton(
+                    child: const Text(ProjectStrings.roundEnd,
+                        style: ProjectTextStyles.buttonLargeTextStyle),
+                    onPressed: () {
+                      globals.currentGame!.setTime();
+                      Navigator.push(
+                          context,
+                          PageTransition(
+                              type: PageTransitionType.fade,
+                              child: const EndOfRound()));
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   void previousPlayer() {
     globals.currentGame!.currentPlayer = globals.currentGame!.currentPlayer - 1;
@@ -52,149 +223,5 @@ class RoundXPageState extends State<RoundXPage> {
           PageTransition(
               type: PageTransitionType.fade, child: const RoundXPage()));
     }
-  }
-
-  //PAGE
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ProjectColors.primarySwatch.shade50,
-      appBar: AppBar(
-        title: const Text(ProjectStrings.appTitle),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      /*
-      appBar: AppBar(
-        title: Text(globals.currentGame!.name as String),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      */
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-                child: Text("Round $roundNum",
-                    style: ProjectTextStyles.pageTitleTextStyle),
-              ),
-              Container(
-                child: Center(
-                  child: Column(
-                    children: [
-                      ListView.builder(
-                        // Widget which creates [ItemWidget] in scrollable list.
-                        shrinkWrap: true,
-                        itemCount: 1, // Number of widgets to be created.
-                        itemBuilder: (context,
-                                itemIndex) => // Builder function for every item with index.
-                            globals.currentGame!.playersView[playerIndex],
-                      )
-                    ],
-                  ),
-                ),
-                width: 350,
-                height: 50,
-                color: ProjectColors.primarySwatch.shade100,
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(130, 10, 0, 0),
-                    child: SizedBox(
-                      width: 100,
-                      height: 50,
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                          hintText: ProjectStrings.roundInputExample,
-                          labelText: ProjectStrings.roundInput,
-                          contentPadding: EdgeInsets.symmetric(
-                              horizontal: 20.0, vertical: 10.0),
-                        ),
-                        onChanged: (String value) {
-                          score = int.parse(value);
-                        },
-                        controller: fieldText,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
-                      child: IconButton(
-                        icon: const Icon(Icons.add_circle),
-                        color: ProjectColors.primarySwatch.shade700,
-                        onPressed: () {
-                          if (score != 0) {
-                            globals.currentGame!.players[playerIndex].points +=
-                                score;
-                            score = 0;
-                            fieldText.clear();
-                            nextPlayer();
-                          }
-                        },
-                      )),
-                ],
-              ),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(25, 200, 0, 0),
-                        child: ElevatedButton(
-                          child: const Text(ProjectStrings.roundPrevious,
-                              style: ProjectTextStyles.buttonLargeTextStyle),
-                          onPressed: () {
-                            previousPlayer();
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 200, 0, 0),
-                        child: ElevatedButton(
-                          child: const Text(ProjectStrings.roundNext,
-                              style: ProjectTextStyles.buttonLargeTextStyle),
-                          onPressed: () {
-                            nextPlayer();
-                          },
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 150, 0, 0),
-                child: ElevatedButton(
-                  child: const Text(ProjectStrings.roundEnd,
-                      style: ProjectTextStyles.buttonLargeTextStyle),
-                  onPressed: () {
-                    globals.currentGame!.setTime();
-                    Navigator.push(
-                        context,
-                        PageTransition(
-                            type: PageTransitionType.fade,
-                            child: const EndOfRound()));
-                  },
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
--- lib/res/globals.dart
+++ lib/res/globals.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:game_saver/res/game_info.dart';
 import 'package:game_saver/res/player_info.dart';
 import 'package:game_saver/res/strings.dart';
+import 'package:game_saver/res/text_styles.dart';
 
 //Game Options Page
 bool gameOptionDefaultTimerON = false;
@@ -15,14 +16,18 @@ bool gameOption5 = false;
 bool gameOption6 = false;
 bool gameOption7 = false;
 
-enum HighLowOptions {high, low}
+enum HighLowOptions { high, low }
 
 //Game list for leaderboard
-List<GameInfo> gameList = [ GameInfo([PlayerInfo( 69, 'Josh')]) ];
+List<GameInfo> gameList = [
+  GameInfo([PlayerInfo(69, 'Josh')])
+];
 
 GameInfo? currentGame;
 
-List<Widget> topScores = [ PlayerScoreWidget(gameList[0].players[0])]; //Initialize with fake player
+List<Widget> topScores = [
+  PlayerScoreWidget(gameList[0].players[0])
+]; //Initialize with fake player
 
 class PlayerScoreWidget extends StatelessWidget {
   const PlayerScoreWidget(this.info, {Key? key}) : super(key: key);
@@ -34,30 +39,46 @@ class PlayerScoreWidget extends StatelessWidget {
     String displayName = info.displayname.toString();
     String score = info.points.toString();
 
-    if (currentGame != null)
-    {
-      info.icon ??= currentGame!.highlowOption == HighLowOptions.high ? Icons.arrow_upward_sharp : Icons.arrow_downward_sharp;
-    }
-    else
-    {
-      info.icon = Icons.arrow_upward_sharp;
-    }
+    // if (currentGame != null)
+    // {
+    //   info.icon ??= currentGame!.highlowOption == HighLowOptions.high ? Icons.arrow_upward_sharp : Icons.arrow_downward_sharp;
+    // }
+    // else
+    // {
+    //   info.icon = Icons.arrow_upward_sharp;
+    // }
 
-    return InkWell( // Enables taps for child and add ripple effect when child widget is long pressed.
-      child: ListTile( // Useful standard widget for displaying something in ListView.
-        leading: Icon(info.icon),
-        title: Text("Player: $displayName                           Score: $score"),
-      )
+    // return InkWell( // Enables taps for child and add ripple effect when child widget is long pressed.
+    //   child: ListTile( // Useful standard widget for displaying something in ListView.
+    //     leading: Icon(info.icon),
+    //     title: Text("Player: $displayName                           Score: $score"),
+    //   )
+    // );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            displayName,
+            style: ProjectTextStyles.appMiniTitleTextStyle,
+          ),
+          Text(
+            score,
+            style: ProjectTextStyles.appMiniTitleTextStyle,
+          ),
+        ],
+      ),
     );
   }
 }
 
 showAlertDialog(BuildContext context, String message) {
-
   // set up the button
   Widget okButton = TextButton(
     child: const Text(ProjectStrings.ok),
-    onPressed: () => { Navigator.of(context).pop() },
+    onPressed: () => {Navigator.of(context).pop()},
   );
 
   // set up the AlertDialog
